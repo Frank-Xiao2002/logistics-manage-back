0b9d282b42a1ebd4ca7453f5cd763118f1e3a8a1
Frank-Xiao2002 <Xiao670127@126.com>
2024-04-29 11:36:40 +0800

    init commit

---
+++ src/main/java/dev/xxj/logistics/LogisticsManageBackApplication.java
@@ -0,0 +1,13 @@
+package dev.xxj.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsManageBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsManageBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/xxj/logistics/LogisticsManageBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.xxj.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsManageBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
