f248834fb461c3df78d051a6ebf90d8cfed1cd11
Frank-Xiao2002 <Xiao670127@126.com>
2024-04-29 11:37:53 +0800

    init commit

--- src/main/java/dev/xxj/logistics/Application.java
+++ src/main/java/dev/xxj/logistics/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LogisticsManageBackApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(LogisticsManageBackApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
